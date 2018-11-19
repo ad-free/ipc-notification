ccd6f5d62a95258fe3e5a441835cbbd295b1f7f2
Anh Duy <duyta8@fpt.com.vn>
2018-11-19 12:45:53 +0700

    [+] Update middleware

--- apiNotifications/settings.py
+++ apiNotifications/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'apps.apis.middleware.ModelAdminReorder',
 ]
 
 ROOT_URLCONF = 'apiNotifications.urls'
