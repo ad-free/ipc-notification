e04ce99a714616149cf4d145ff2933b3bf77cddf
Anh Duy <duyta8@fpt.com.vn>
2018-12-03 18:16:28 +0700

    [+] Remove source code

--- apps/apis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.apis import views
-
-urlpatterns = [
-	path('notification/register/', views.api_notification_register, name='api_notification_register'),
-	path('notification/update/', views.api_notification_update, name='api_notification_update'),
-	path('notification/push/', views.api_notification_push, name='api_notification_push'),
-]
