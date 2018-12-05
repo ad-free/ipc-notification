71d1bee1feba680417fdb1ebdc2130a48655aff1
Anh Duy <duyta8@fpt.com.vn>
2018-12-05 21:21:49 +0700

    [+] Insert group field

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -13,7 +13,7 @@ class CustomerAdmin(UserAdmin):
 	fieldsets = (
 		(None, {'fields': ('username', 'password')}),
 		(_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-		(_('Permissions'), {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+		(_('Permissions'), {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups')}),
 	)
 	add_fieldsets = (
 		(None, {'classes': ('wide',), 'fields': ('username', 'password1', 'password2')}),
