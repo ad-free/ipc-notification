1d5b8ffa31cf499809c4dced32521f7b2f00160f
Tran Anh Duy <duyta8@fpt.com.vn>
2019-01-23 14:07:40 +0700

    Renew source code

--- apps/apis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from apps.apis import views
-
-urlpatterns = [
-	path('notification/register/', views.api_notification_register, name='api_notification_register'),
-	path('notification/update/', views.api_notification_update, name='api_notification_update'),
-	path('notification/delete/', views.api_notification_delete, name='api_notification_delete'),
-	path('notification/active/', views.api_notification_active, name='api_notification_active'),
-]
