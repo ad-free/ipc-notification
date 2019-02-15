2e3b8599d146f367822043263ab4f373b2c4b77d
Tran Anh Duy <duyta8@fpt.com.vn>
2019-02-15 10:39:22 +0700

    [+] Update user status via Javascript [+] Update settings file

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
+from django.utils.html import format_html
 
 from .models import Customer, Staff
 
 
 @admin.register(Customer)
 class CustomerAdmin(UserAdmin):
+	change_list_template = 'admin/users.html'
 	fieldsets = (
 		(None, {'fields': ('username', 'password')}),
 		(_('Personal info'), {'fields': ('first_name', 'last_name', 'email',)}),
 	)
-	list_display = ('username', 'email', 'last_login')
+	list_display = ('username', 'email', 'last_login', 'is_online',)
 	list_filter = ['date_joined']
 	exclude = ('groups',)
+	readonly_fields = ('is_online',)
 
 	def get_queryset(self, request):
 		qs = super(CustomerAdmin, self).get_queryset(request)
@@ -26,6 +31,21 @@ class CustomerAdmin(UserAdmin):
 		obj.is_staff = False
 		obj.is_superuser = False
 		obj.save()
+
+	def get_server_info(self, obj):
+		return mark_safe('<div style="display: none" class="server-info" data-host="{host}" data-port="{port}"></div>'.format(
+				host=settings.API_QUEUE_HOST,
+				port=settings.API_QUEUE_PORT
+		))
+
+	get_server_info.allow_tags = True
+
+	class Media:
+		static_url = getattr(settings, 'STATIC_URL')
+		js = [
+			static_url + 'assets/js/mqtt/mqttws31.js',
+			static_url + 'assets/js/status.js',
+		]
 
 
 @admin.register(Staff)
---
+++ apps/users/templatetags/server_info.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag
+def server_info(name):
+	if name == 'host':
+		return getattr(settings, 'API_QUEUE_HOST', '')
+	elif name == 'port':
+		return getattr(settings, 'API_QUEUE_WEBSOCKET_PORT', '')
+	elif name == 'topic':
+		return getattr(settings, 'USER_TOPIC_STATUS', '')
+	return ''
