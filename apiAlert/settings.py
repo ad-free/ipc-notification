7a6c7df2812d4b89d18fbe262c36196f4e86840e
Tran Anh Duy <duyta8@fpt.com.vn>
2019-02-25 13:46:41 +0700

    [+] Create a language file

--- apiAlert/settings.py
+++ apiAlert/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,6 +136,10 @@ CONFIG_ROOT = os.path.join(BASE_DIR, 'config')
 CONFIG_URL = '/config/'
 
 ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+LOCALE_PATHS = [
+	os.path.join(BASE_DIR, 'locale')
+]
 
 try:
 	from apiAlert.local_settings import *
